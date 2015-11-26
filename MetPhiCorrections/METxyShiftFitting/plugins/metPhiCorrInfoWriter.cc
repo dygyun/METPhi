efc1cba8332492ca08b27f4ff7291133aec58327
Dooyeon Gyun <dygyun@Dooyeonui-MacBook-Air.local>
2015-11-26 19:24:05 +0900

    sub-metphi

---
+++ MetPhiCorrections/METxyShiftFitting/plugins/metPhiCorrInfoWriter.cc
@@ -0,0 +1,167 @@
+#include "MetTools/MetPhiCorrections/plugins/metPhiCorrInfoWriter.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "DataFormats/Common/interface/View.h"
+#include "DataFormats/Common/interface/Association.h"
+#include <string>
+#include "TMath.h"
+std::string namePostFix (int varType) {
+  if (varType==0) return std::string("multiplicity");
+  if (varType==1) return std::string("ngoodVertices");
+  if (varType==2) return std::string("sumPt");
+  return std::string("unknown");
+}
+int metPhiCorrInfoWriter::translateTypeToAbsPdgId( reco::PFCandidate::ParticleType type ) {
+  switch( type ) {
+  case reco::PFCandidate::ParticleType::h: return 211; // pi+
+  case reco::PFCandidate::ParticleType::e: return 11;
+  case reco::PFCandidate::ParticleType::mu: return 13;
+  case reco::PFCandidate::ParticleType::gamma: return 22;
+  case reco::PFCandidate::ParticleType::h0: return 130; // K_L0
+  case reco::PFCandidate::ParticleType::h_HF: return 1; // dummy pdg code
+  case reco::PFCandidate::ParticleType::egamma_HF: return 2; // dummy pdg code
+  case reco::PFCandidate::ParticleType::X:
+  default: return 0;
+  }
+}
+metPhiCorrInfoWriter::metPhiCorrInfoWriter( const edm::ParameterSet & cfg ): 
+  pflow_ ( cfg.getUntrackedParameter< edm::InputTag >("srcPFlow") ),
+  vertices_ ( cfg.getUntrackedParameter< edm::InputTag >("vertexCollection") ),
+  moduleLabel_(cfg.getParameter<std::string>("@module_label"))
+{
+  edm::Service<TFileService> fs;
+
+  cfgCorrParameters_ = cfg.getParameter<std::vector<edm::ParameterSet> >("parameters");
+//  etaNBins_.clear();
+  etaMin_.clear();
+  etaMax_.clear();
+  type_.clear();
+  varType_.clear();
+  nbins_.clear();
+  counts_.clear();
+  sumPt_.clear();
+  //METx_.clear();
+  MEx_.clear();
+  MEy_.clear();
+  for (std::vector<edm::ParameterSet>::const_iterator v = cfgCorrParameters_.begin(); v!=cfgCorrParameters_.end(); v++) {
+    int etaNBins = v->getParameter<int>("etaNBins");
+    double etaMin = v->getParameter<double>("etaMin");
+    double etaMax = v->getParameter<double>("etaMax");
+    int phiNBins = v->getParameter<int>("phiNBins");
+    double phiMin = v->getParameter<double>("phiMin");
+    double phiMax = v->getParameter<double>("phiMax");
+    int nMin = v->getParameter<int>("nMin");
+    int nMax = v->getParameter<int>("nMax");
+    int nbins = v->getParameter<double>("nbins");
+//    etaNBins_.push_back(etaNBins);
+    etaMin_.push_back(etaMin);
+    etaMax_.push_back(etaMax);
+    nbins_.push_back(nbins);
+    int varType(v->getParameter<int>("varType"));
+    varType_.push_back(varType);
+    type_.push_back(v->getParameter<int>("type"));
+    counts_.push_back(0);
+    sumPt_.push_back(0.);
+    MEx_.push_back(0.);
+   // METx_.push_back(0.);
+    MEy_.push_back(0.);
+//    std::cout<<" n/min/max "<<nbins<<" "<<etaMin<<" "<<etaMax<<std::endl;
+    profile_x_.push_back(fs->make<TProfile>(std::string(moduleLabel_).append("_").append(namePostFix(varType)).append("_").append(v->getParameter<std::string>("name")).append("_Px").c_str(),"Px", nbins, nMin, nMax, -300,300));
+    profile_y_.push_back(fs->make<TProfile>(std::string(moduleLabel_).append("_").append(namePostFix(varType)).append("_").append(v->getParameter<std::string>("name")).append("_Py").c_str(),"Py", nbins, nMin, nMax, -300,300));
+
+    occupancy_.push_back(fs->make<TH2F>(std::string(moduleLabel_).append("_").append(namePostFix(varType)).append("_").append(v->getParameter<std::string>("name")).append("_occupancy").c_str(),"occupancy",  etaNBins, etaMin, etaMax, phiNBins, phiMin, phiMax));
+    energy_.push_back(fs->make<TH2F>(std::string(moduleLabel_).append("_").append(namePostFix(varType)).append("_").append(v->getParameter<std::string>("name")).append("_energy").c_str(),"energy",           etaNBins, etaMin, etaMax, phiNBins, phiMin, phiMax));
+    pt_.push_back(fs->make<TH2F>(std::string(moduleLabel_).append("_").append(namePostFix(varType)).append("_").append(v->getParameter<std::string>("name")).append("_pt").c_str(),"pt",                       etaNBins, etaMin, etaMax, phiNBins, phiMin, phiMax));
+
+    variable_.push_back(fs->make<TH1F>(std::string(moduleLabel_).append("_").append(namePostFix(varType)).append("_").append(v->getParameter<std::string>("name")).append("_variable").c_str(),"variable", nbins, nMin, nMax));
+    METx_.push_back(fs->make<TH1F>(std::string(moduleLabel_).append("_").append(namePostFix(varType)).append("_").append(v->getParameter<std::string>("name")).append("_METx").c_str(),"METx", 100,-100,100));
+    METy_.push_back(fs->make<TH1F>(std::string(moduleLabel_).append("_").append(namePostFix(varType)).append("_").append(v->getParameter<std::string>("name")).append("_METy").c_str(),"METy", 100,-100,100));
+    METphi_.push_back(fs->make<TH1F>(std::string(moduleLabel_).append("_").append(namePostFix(varType)).append("_").append(v->getParameter<std::string>("name")).append("_METphi").c_str(),"METphi", 60,-TMath::Pi(),TMath::Pi() ));
+  }
+}
+
+void metPhiCorrInfoWriter::analyze( const edm::Event& evt, const edm::EventSetup& setup) {
+  //get primary vertices
+  edm::Handle<edm::View<reco::Vertex> > hpv;
+  try {
+    evt.getByLabel( vertices_, hpv );
+  } catch ( cms::Exception & e ) {
+    std::cout <<"[metPhiCorrInfoWriter] error: " << e.what() << std::endl;
+  }
+  std::vector<reco::Vertex> goodVertices;
+  for (unsigned i = 0; i < hpv->size(); i++) {
+    if ( (*hpv)[i].ndof() > 4 &&
+       ( fabs((*hpv)[i].z()) <= 24. ) &&
+       ( fabs((*hpv)[i].position().rho()) <= 2.0 ) )
+       goodVertices.push_back((*hpv)[i]);
+  }
+  int ngoodVertices = goodVertices.size();
+
+  for (unsigned i=0;i<counts_.size();i++) {
+    counts_[i]=0;
+    sumPt_[i]=0;
+    //METx_[i]=0.;
+    MEx_[i]=0.;
+    MEy_[i]=0.;
+  } 
+  edm::Handle<edm::View<reco::Candidate> > particleFlow;
+  evt.getByLabel(pflow_, particleFlow);
+  for (unsigned i = 0; i < particleFlow->size(); ++i) {
+    const reco::Candidate& c = particleFlow->at(i);
+    for (unsigned j=0; j<type_.size(); j++) {
+//      if (abs(c.pdgId())==211) {
+//        std::cout<<"cand pdgId "<<c.pdgId()<<" testing type:"<<type_[j]<<" translated to pdg:"<<translateTypeToAbsPdgId(reco::PFCandidate::ParticleType(type_[j]))<<std::endl;
+//      }
+      if (abs(c.pdgId())== translateTypeToAbsPdgId(reco::PFCandidate::ParticleType(type_[j]))) {
+       // if (j != 4) {continue;}
+       if ( (c.eta()>etaMin_[j]) and (c.eta()<etaMax_[j]) )
+      // if ((c.eta()> 1.392 && c.eta()< 3 )) 
+        {
+          counts_[j]+=1;
+          sumPt_[j]+=c.pt();
+          MEx_[j]-=c.px();
+          MEy_[j]-=c.py();
+         //MEx_[j]=MEx_[j]-c.px();
+          pt_[j]->Fill(c.eta(), c.phi(), c.pt()); 
+          energy_[j]->Fill(c.eta(), c.phi(), c.energy()); 
+          occupancy_[j]->Fill(c.eta(), c.phi()); 
+    //     std::cout<<"cand pdgId "<<c.pdgId()<<" testing type:"<<type_[j]<<" translated to pdg:"<<translateTypeToAbsPdgId(reco::PFCandidate::ParticleType(type_[j])) <<std::endl;
+        }  ///////////min Eta max range..... metx, mety
+      
+      }//////pdgId  metx, mety
+    }  ////////particle type like h, h0....
+//////////////////////////////////////////////////////////////
+ 
+  }
+  for (std::vector<edm::ParameterSet>::const_iterator v = cfgCorrParameters_.begin(); v!=cfgCorrParameters_.end(); v++) {
+    unsigned j=v-cfgCorrParameters_.begin();
+//    std::cout<<"j "<<j<<" "<<v->getParameter<std::string>("name")<<" varType "<<varType_[j]<<" counts "<<counts_[j]<<" sumPt "<<sumPt_[j]<<" nvtx "<<ngoodVertices<<" "<<MEx_[j]<<" "<<MEy_[j]<<std::endl;
+    if (varType_[j]==0) {
+      METx_[j]->Fill(MEx_[j]);
+      METy_[j]->Fill(MEy_[j]);
+      double metPhi = (TMath::ATan2(MEy_[j],MEx_[j]) );
+      METphi_[j]->Fill(metPhi);
+      profile_x_[j]->Fill(counts_[j], MEx_[j]);//Profile of MEx vs PF candidate multiplicity
+      profile_y_[j]->Fill(counts_[j], MEy_[j]);
+      variable_[j]->Fill(counts_[j]);
+    } 
+    //std::cout<<" METx "<< METx_[j] <<"----MEtPhi "<< METphi_[j]  <<"______eta "<< c.eta() <<std::endl;
+    if (varType_[j]==1) {
+      profile_x_[j]->Fill(ngoodVertices, MEx_[j]);
+      profile_y_[j]->Fill(ngoodVertices, MEy_[j]);
+      variable_[j]->Fill(ngoodVertices);
+    } 
+    if (varType_[j]==2) {
+      profile_x_[j]->Fill(sumPt_[j], MEx_[j]);
+      profile_y_[j]->Fill(sumPt_[j], MEy_[j]);
+      variable_[j]->Fill(sumPt_[j]);
+    }
+  }
+}
+//define this as a plug-in
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(metPhiCorrInfoWriter);
+
---
+++ MetPhiCorrections/METxyShiftFitting/plugins/metPhiCorrInfoWriter.h
@@ -0,0 +1,44 @@
+#ifndef HEPHECommonTools_metPhiCorrInfoWriter_H
+#define HEPHECommonTools_metPhiCorrInfoWriter_H
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+// #include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"                                                                               
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+
+#include <string>
+#include <vector>
+#include <TProfile.h>
+#include <TH2F.h>
+
+class metPhiCorrInfoWriter : public edm::EDAnalyzer {
+public:
+  metPhiCorrInfoWriter( const edm::ParameterSet & );
+
+private:
+  edm::InputTag pflow_;
+
+  void analyze( const edm::Event& , const edm::EventSetup& );
+  edm::InputTag vertices_;
+  std::string moduleLabel_;
+  std::vector<edm::ParameterSet> cfgCorrParameters_;
+  std::vector<TProfile* > profile_x_ , profile_y_;
+  std::vector<TH2F* > occupancy_ , energy_, pt_;
+ // std::vector<TH1F* > variable_;
+  std::vector<TH1F* > variable_, METx_, METy_, METphi_;
+
+  std::vector<double> etaMin_, etaMax_, MEx_, MEy_, sumPt_;
+  //std::vector<double> etaMin_, etaMax_, MEx_, MEy_, sumPt_, METx_, METy_;
+  std::vector<int> type_, varType_, nbins_, counts_, etaNBins_;
+
+  static int translateTypeToAbsPdgId( reco::PFCandidate::ParticleType type );
+
+};
+
+#endif
+
+
